ee01c5f94b168667490e3b798d8023270114ddd1
Nathaniel J. Smith <njs@pobox.com>
2015-07-11 16:41:47 -0500

    Initial commit, fragments of partially written code

---
+++ sempervirens.py
@@ -0,0 +1,205 @@
+# This file is part of the Open Telemetry Project
+# Copyright (C) 2015 Nathaniel Smith <njs@pobox.com>
+# See file LICENSE.txt for license information.
+
+import os
+import os.path
+import socket
+import json
+import threading
+from collections import defaultdict
+
+import appdirs
+import requests
+
+DIRECTORY_NAME = "open-telemetry-project"
+
+DISABLE_ENVVAR = "OTP_DISABLE"
+
+# Cross-platform directory nonsense:
+# One version (has all-versions wheel on pypi):
+#    https://github.com/ActiveState/appdirs/blob/master/appdirs.py
+# Critiques:
+#    - Supposedly the correct way to get the OS X directory is
+#         -[NSFileManager URLsForDirectory: NSApplicationSupportDirectory inDomains: NSUserDomainMask]
+#      whereas the above hardcodes ~/Library/Application Support/
+#    - The unixy code doesn't take any care to be robust against missing HOME
+#      (Or no, wait: os.expanduser falls back on getpwduid, so I guess that's
+#      correct.)
+#    - The unixy code ignores the rule that if an XDG_*_HOME variable is empty
+#      or contains a non-absolute path, then you should ignore it. See
+#      http://standards.freedesktop.org/basedir-spec/latest/ar01s02.html
+#
+# Annoyingly, it's not clear to me that Windows or OS X even *have* an
+# equivalent of /etc for per-machine config. I guess OS X does from this link?
+#   https://apple.stackexchange.com/questions/76611/cant-access-etc-folder-in-os-x-mountain-lion
+# Yeah, must be:
+#   https://apple.stackexchange.com/search?q=%2Fetc
+#
+# Maybe we can live with an envvar for global disablement at least for now.
+
+# DATA_DIR is something like
+#   ~/.local/share/open-telemetry-project/
+#
+# It contains:
+#   /consent
+#     {"consent-given": True,
+#      "consent-type": {"program": "IPython", "version": "..."},
+#      "consent-date": "2015-12-01"}
+#   /install-id
+#     16 bytes
+#     this should permissions 0600
+#   /otp-python/   -- ours to play with
+
+class OTP(object):
+    def __init__(self):
+        self.enabled = False
+        # The main OTP directory, e.g. ~/.local/share/open-telemetry-project
+        self.otp_path = None
+        # The directory we use for storing things, e.g.
+        #    ~/.local/share/open-telemetry-project/otp-python/$HOSTNAME/
+        self.data_path = None
+
+        # {project_id:
+        #   {key:
+        #     {value: counter}
+        self.stats = defaultdict(lambda:
+                                 defaultdict(lambda:
+                                             defaultdict(float)))
+
+        if DISABLE_ENVVAR in os.environ:
+            return
+
+        user_data_path = appdirs.user_data_dir()
+        if not os.path.isdir(user_data_path):
+            return
+
+        self.otp_path = os.path.join(user_data_path, DIRECTORY_NAME)
+        # If the otp_path doesn't even exist, then the user has definitely not
+        # consented.
+        if not os.path.isdir(self.otp_path):
+            return
+
+        # if we do not own directory, then bail out
+        XX
+
+        self.otp_python_dir = os.path.join(self.otp_dir,
+                                           "otp-python",
+                                           socket.gethostname())
+
+
+
+    def increment(self, project_id, key, value, count=1):
+        self.stats[project_id][key][value] += count
+
+    def register_poll_callback(self, project_id, fn):
+        # project_id is so we can measure how slow fn() is and complain to the
+        # right people if it's bad.
+        XX
+
+    def record_consent(self, version, consented):
+
+
+    def _ensure_directory(self):
+        if self.data_dir is None:
+            return
+
+        if not os.path.exists(self.data_dir):
+
+
+
+# Snapshotting is tricky -- what if someone comes along and uploads a partial
+# snapshot? I guess we can't prevent that, and we're going to be rolling up
+# sessions anyway, so eh. Just reset our internal stats (including "number of
+# sessions") to 0 whenever we snapshot.
+
+
+# acquire a lock to create the daily rollup
+#   on temporary failure, move the rollup back into the regular directory like
+#     it's a single session
+#   if there are >1 week old daily rollups, (attempt to) retrieve their
+#     contents and put them back into circulation
+#
+# locking:
+#   probably the only reliable method on NFS:
+#     - create a unique file
+#     - link(unique file, common lock file name)
+#     - stat(unique file)
+#     - if unique file's link count is 2, then we own the lock
+#   of course, SMB is another matter...
+# BETTER IDEA: give each host its own unique data directory
+#   open-telemetry-project/otp-py/branna/pending/...
+#                                       /rollup-<day>
+# of course, this breaks whenever a computer changes its hostname:
+#   https://stackoverflow.com/questions/4271740/how-can-i-use-python-to-get-the-system-hostname#comment16118145_4271755
+# Sources of unique ids:
+#  - on linux with systemd (and others?): /etc/machine-id
+#  - on windows there's
+#       HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\MachineGuid
+#    (which is supposed to be unique, but can fail to be if someone uses a
+#    non-standard method of cloning new machines, "not running sysprep")
+#    Of course we could also just store our own id under HKEY_LOCAL_MACHINE
+#  - On OS X there's gethostuuid()
+# maybe we should just use the hostname, plus fcntl.lockf / msvcrt.locking.
+# ...does fcntl locking work between processes on the same machine on nfs?
+#
+# -- lock /tmp/otp-<username>? dangerous if someone else grabs it first though
+#   (and named mutexes are basically equivalent to this on windows)
+#
+# if each ping has an id attached, and we create this id by something like
+#   hash(secret, date, hostname, installid), then this should reliably let us
+#   see when two processes both tried to read the same day
+#   and double-counting within a single user isn't *that* big a deal anyway
+#   - better: give install-id, sub-install-id, reporting-period, and the
+#     sub-install id is just an arbitrary nonce which should make that triple
+#     unique under ordinary circumstances.
+#
+# docs say:
+#   - on FreeBSD: mount_nfs(8) says: you can have either local locking (with
+#     "nolockd" / -L), or server-side locking
+#   - on Linux: nfs(5) says: "lock" -> use NLM, "nolock" -> local locking
+#   - on OSX: mount_nfs(8) says: you can choose real locks, local locks, or to
+#     have all lock operations return ENOTSUP
+#     OSX tends to assume file locking works though I think, and who mounts
+#     their OSX home directory over NFS anyway?
+#
+# after writing: check to see if there are previous day/week/whatever records
+# if so spawn the cleaner, which will try to grab the machine-dir lock
+#
+# we also need some sort of permanent failure mode I guess, to prevent growing
+#   unboundedly? maybe discard any single log file that is larger than a fixed
+#   size (and increment a counter saying that we've done so)?
+#   or just throw away anything that's more than a month out?
+
+
+# stuff in ping:
+#   - install-id
+#   - beginning of earliest session covered
+#   - end of latest session covered
+#
+
+# Things we should just go ahead and log ourselves:
+#   platform type (sys.platform?)
+#   python version
+#   python distributor if we can figure it out?
+#   32- versus 64-bit
+#   other CPUID stuff? maybe better leave that to numpy; very difficult to do
+#     without compiled code.
+#   our own version number!
+#   versions for some particular known packages
+#      - numpy, scipy, matplotlib, pandas, IPython
+#      eh, or maybe not. we don't even see them unless we get imported (I
+#      guess that will happen via IPython, but...), and gives a skewed idea of
+#      things...
+#
+#   our overhead: delay added to shutdown, anything else we can log cheaply?
+#   ...delay added to shutdown is hard given that by the time we've measured
+#      it, it's too late to record it...
+#   time spent calling poll() functions would make sense to count, though
+
+
+if __name__ == "__main__":
+    # Have "opt-in" and "opt-out" commands
+    # and "status" -- opted in/out + directory size
+    # and where the directory is so people can browse around if they want
+    pass
---
+++ server.py
@@ -0,0 +1,197 @@
+# This file is part of the Open Telemetry Project
+# Copyright (C) 2015 Nathaniel Smith <njs@pobox.com>
+# See file LICENSE.txt for license information.
+
+# This file requires Python 3.4+
+
+import sys
+import time
+import ssl
+import ipaddress
+
+import asyncio
+from aiohttp import web
+import aiodns
+
+# XX numpy + mmap to make a scoreboard, return on GET /?
+# make infrastructure to fork off processes?
+
+# Helper for doing reverse-DNS lookups. Unnecessary on Py3.5+.
+def reverse_pointer(ipaddr_string):
+    ipaddr = ipaddress.ip_address(ipaddr_string)
+    if sys.version_info >= (3, 5):
+        return ipaddr.reverse_pointer
+    else:
+        if isinstance(ipaddr, ipaddress.IPv4Address):
+            return (".".join(reversed(ipaddr.exploded.split(".")))
+                    + ".in-addr.arpa")
+        else:
+            assert isinstance(ipaddr, ipaddress.IPv6Address)
+            nibbles = [char for char in self.exploded if char != ":"]
+            return ".".join(reversed(nibbles)) + ".ip6.arpa"
+
+class Server(object):
+    def __init__(self, host, port, *, trust_forwarded_for, ssl=None):
+        self.host = host
+        self.port = port
+        self.trust_forwarded_for = trust_forwarded_for
+        self.ssl = ssl
+
+        self.loop = asyncio.get_event_loop()
+        self.resolver = aiodns.DNSResolver(loop=self.loop)
+
+        self.start_time = time.time()
+        self.processed = 0
+        self.avg_latency_sec = 0
+
+        # URL: include some basic metadata inside the URL -- mozilla found
+        # they needed to do this to allow fast sharding at high data
+        # rates. maybe the instance-id and the submission-nonce? (mozilla's
+        # metadata is which app, which version of it, which ping type, etc.)
+        # -- https://bugzilla.mozilla.org/show_bug.cgi?id=860846 include a
+        # version number of course.
+        #   POST incoming.otp.whereever.org/1.0/submit/<instance-id>/<submission-nonce>
+        #   POST incoming.otp.whereever.org/1.0/opt-out/<instance-id>
+
+        self.app = web.Application(loop=self.loop)
+        self.app.router.add("GET", "/", self.root)
+        self.app.router.add("POST", "/1.0/submit/{install_id}", self.submit)
+        self.app.router.add("POST", "/1.0/opt-out/{install_id}", self.opt_out)
+
+
+    @asyncio.coroutine
+    def start(self, host, port, ssl=None):
+        # XX: create our own socket, use SO_REUSEPORT so that multiple servers
+        # can all attach to the same port and the kernel will round-robin
+        # between them, and then pass as sock= argument
+        # len(os.sched_getaffinity(0)) is number of available CPUs
+        return self.loop.create_server(self.app.make_handler(),
+                                       self.host, self.port, ssl=self.ssl)
+
+    @asyncio.coroutine
+    def _rdns(self, ipaddr_string):
+        # Returns a future yielding a list of domain names reverse-resolved
+        # from the given ip
+        rptr = reverse_pointer(ipaddr_string)
+        try:
+            return (yield from self.resolver.query(rptr, "PTR"))
+        except aiodns.error.DNSError:
+            return []
+
+    def _request_ip(self, request):
+        if self.trust_forwarded_for:
+            return request.headers["x-forwarded-for"]
+        else:
+            (host, port) = request.transport.get_extra_info("peername")
+            return host
+
+    @asyncio.coroutine
+    def root(self, request):
+        # return some stats
+        XX
+
+    @asyncio.coroutine
+    def submit(self, request):
+        ip = self._request_ip(request)
+
+
+if __name__ == "__main__":
+    ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    # As of 3.4 the default context allows TLSv1, but we don't need it and
+    # it's disrecommended by e.g. Mozilla. Since this makes us strictly more
+    # restrictive, it's fail-safe even if Python later tightens up their
+    # defaults.
+    #   https://wiki.mozilla.org/Security/Server_Side_TLS
+    ssl.options |= ssl.OP_NO_TLSv1
+    # We could also override the cipher string if we wanted, but then we are
+    # on the hook to maintain it...
+
+
+
+
+# Server-side thoughts:
+#   10 million requests / day = 116 requests/second
+#
+#   https://github.com/KeepSafe/aiohttp
+#   http://blog.gmludo.eu/2015/02/macro-benchmark-with-django-flask-and-asyncio.html
+#   https://github.com/saghul/aiodns
+
+# As of May 2015, Mozilla is handling peaks of ~1.8e6 pings/hour (= ~500
+# requests/second on average over that hour), with ~31 GiB data received
+# during that hour. Which is. Umm. A lot. (That's ~18 KiB/ping.)
+# http://ec2-50-112-66-71.us-west-2.compute.amazonaws.com:4352/#sandboxes/TelemetryChannelMetrics60DaysAggregator/outputs/TelemetryChannelMetrics60DaysAggregator.ALL.cbuf
+# more stats stuff:
+# http://ec2-50-112-66-71.us-west-2.compute.amazonaws.com:4352/#sandboxes
+#
+# Their server code:
+#   https://github.com/mozilla/telemetry-server
+#   https://github.com/mozilla/telemetry-server/blob/master/http/server.js
+# they reject any POST without content-length, they have a max data length
+# they'll accept (and if something exceeds this they respond with a 202 so it
+# won't get resent), they actual pings into files that get rotated at max size
+# or max time, and they log some metadata about each request (url, bytes
+# written, etc.) to a separate file... the main data file is in a fixed-width
+# int binary format. They spawn 1 single-threaded node.js server per CPU on a
+# server.
+#
+# Not sure how to do storage -- would rather not just write to a single frail
+# disk really. Could dump directly into cloud files? But they are not designed
+# for high write rates -- each container (= "bucket") is limited to 100 object
+# write requests / second, and they say "You can’t expect to write to the same
+# object 20 times per second" -- http://www.rackspace.com/knowledge_center/article/best-practices-for-using-cloud-files
+# s3 appears to be more scalable to high-write workloads:
+#   https://docs.aws.amazon.com/AmazonS3/latest/dev/request-rate-perf-considerations.html
+# Neither allow appends, though -- just creation of new objects.
+# On Amazon, SimpleDB is one approach.
+#    https://aws.amazon.com/simpledb/
+#    -- see "Logging" section
+# Postgres could also work -- they do high-throughput transaction batching (so
+# one fsync for many transactions) since 9.2.
+# dropping into kafka I guess does allow to tee to both disk and some realtime
+# analytics aggregation
+# it does sound like kafka is pretty much the tool that's designed to deal
+# with this problem. but even kafka expires data based on time, not whether
+# it's been processed.
+#
+# could batch up writes to cloud storage -- once every 30 s or whatever.
+# how durable is block storage?
+#   "customers are strongly encouraged to implement a RAID level 1 (mirror)
+#   configuration across multiple volumes to protect against data loss in the
+#   event of a storage node failure."
+# so that makes sense I guess.
+# we could mount 2-3 block devices in RAID-1, stream stuff out to them
+# append-style, and have a thread in the background that just fsync's
+# continuously, and we don't reply success until after the next fsync has
+# completed.
+#   $0.12/GB/month, min 75 GB
+#     -> $9/mo for the cheapest block device
+#
+# other storage options?
+# - mongodb: no. https://aphyr.com/posts/284-call-me-maybe-mongodb
+# - could just forward data to 3 more http servers each of whom write it to
+#   disk
+#
+# aggregation:
+# - for each time period, number of opt-outs vs. number of opt-ins -> ratio
+# - let's put opt-in period into the opt-in metadata, to make this easily
+#   available for imputation without having to keep records on ever installid
+# - for each project, all (TLD, installid) pairs
+#   - and counts of these uniques by TLD for different time periods
+# - for each
+#
+# I guess we'll use aiohttp, with something like:
+#   16 bytes magic
+# then for each record:
+#   u4 + string: ip address (could be ipv6)
+#   ?? u4 + string: dns name
+#   u4 + string: url
+#   u8: time_t
+#   u4 + string: the json payload (unvalidated)
+# and then roll over the log every day or so. Or for opt-outs,
+#   16 bytes magic
+# then for each record:
+#   ?? u4 + string: ip address
+#   ?? u4 + string: dns name
+#   u4 + string: url
+#   u8: time_t
+#   u4 + string: the json payload (unvalidated)
